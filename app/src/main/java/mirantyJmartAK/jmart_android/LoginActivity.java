2e611604aba682579f031a6cd07a1a752405fa30
MirantyAnjaniPutri <mirantyanjaniputri@ui.ac.id>
2021-11-27 11:29:36 +0700

    CS Modul 9

--- app/src/main/java/mirantyJmartAK/jmart_android/LoginActivity.java
+++ app/src/main/java/mirantyJmartAK/jmart_android/LoginActivity.java
@@ -1,13 +1,46 @@
 package mirantyJmartAK.jmart_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import mirantyJmartAK.jmart_android.model.Account;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final Gson gson = new Gson();
+    private static Account loggedAccount = null;
+
+    public static Account getLoggedAccount () {
+        return loggedAccount;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.login);
+        EditText email = (EditText)findViewById(R.id.loginEmail);
+        EditText password = (EditText)findViewById(R.id.loginPass);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().equals(loggedAccount.email) &&
+                        password.getText().toString().equals(loggedAccount.password)) {
+                    Toast.makeText(getApplicationContext(), "Login Berhasil!", Toast.LENGTH_SHORT).show();
+                    Intent moveIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(moveIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Informasi login tidak sesuai.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/mirantyJmartAK/jmart_android/RegisterActivity.java
+++ app/src/main/java/mirantyJmartAK/jmart_android/RegisterActivity.java
@@ -2,13 +2,33 @@ package mirantyJmartAK.jmart_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.login);
+        EditText name = (EditText)findViewById(R.id.name);
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText password = (EditText)findViewById(R.id.pass);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(moveIntent);
+
+            }
+        });
     }
 }
